4a9fecbb542b75fd853e6e4ef989bcd539354968
BJIKEN <148158063+BJIKEN@users.noreply.github.com>
2023-11-04 01:42:29 +0000

    Update __init__.py
    made changes to make the tool its name correctly when hovered over, changed priority/order in toolbox (weight)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
-from . import TestTool
+from . import ModelScaleTool
 
 from UM.i18n import i18nCatalog
 i18n_catalog = i18nCatalog("uranium")
@@ -9,13 +9,13 @@ i18n_catalog = i18nCatalog("uranium")
 def getMetaData():
     return {
         "tool": {
-            "name": i18n_catalog.i18nc("@action:button", "Move"),
-            "description": i18n_catalog.i18nc("@info:tooltip", "Move Model"),
-            "icon": "ArrowFourWay",
-            "tool_panel": "TestTool.qml",
-            "weight": -1
+            "name": i18n_catalog.i18nc("@action:button", "Scale Model"),
+            "description": i18n_catalog.i18nc("@info:tooltip", "Scale the model"),
+            "icon": "ModelScale_icon",
+            "tool_panel": "ModelScaleTool.qml",
+            "weight": 19 # set to random high value to make sure tool appears at bottom of toolbar
         }
     }
 
 def register(app):
-    return { "tool": TestTool.TestTool() }
+    return { "tool": ModelScaleTool.ModelScaleTool() }
